1c4f13abcefad6cc4e5f3cc4643deb4a0c11ae9a
Kristiyan Borisov <genkoph@protonmail.com>
2023-02-04 14:27:54 +0200

    chore: format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { type NextPage } from 'next'
-import Head from 'next/head'
+import { type NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
@@ -12,21 +12,38 @@ const Home: NextPage = () => {
       <section className="py-24 flex items-center min-h-screen justify-center bg-white">
         <div className="mx-auto max-w-[43rem]">
           <div className="text-center">
-            <p className="text-lg font-medium leading-8 text-indigo-600/95">Lorem ipsum dolor sit amet.</p>
-            <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
-            <p className="mt-3 text-lg leading-relaxed text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, eligendi. Sint ipsa officia a alias nisi optio saepe voluptatibus unde.</p>
+            <p className="text-lg font-medium leading-8 text-indigo-600/95">
+              Lorem ipsum dolor sit amet.
+            </p>
+            <h1 className="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </h1>
+            <p className="mt-3 text-lg leading-relaxed text-slate-400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+              eligendi. Sint ipsa officia a alias nisi optio saepe voluptatibus
+              unde.
+            </p>
           </div>
 
           <div className="mt-6 flex items-center justify-center gap-4">
-            <a href="#" className="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Get started for free</a>
-            <a href="#" className="transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"> Request a demo </a>
+            <a
+              href="#"
+              className="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700"
+            >
+              Get started for free
+            </a>
+            <a
+              href="#"
+              className="transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"
+            >
+              {" "}
+              Request a demo{" "}
+            </a>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
