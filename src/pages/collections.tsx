210280b25590b8deb6041ea07df73372f8117368
Kiril Dimitrov <kiril.dimitrov@mkit.io>
2023-02-18 15:54:42 +0200

    feat: collections page

---
+++ src/pages/collections.tsx
@@ -0,0 +1,67 @@
+import { GetStaticPropsContext, type NextPage } from "next";
+import Head from "next/head";
+import Client from "shopify-buy";
+
+import { type Collection } from "src/types/shared";
+import { sanitizeShopifyId } from "src/utils";
+
+interface Props {
+    collectionList: Collection[];
+}
+
+const Collections: NextPage<Props> = ({ collectionList }) => {
+    return (
+        <>
+            <Head>
+                <title>Ceramicss - Home</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <section className="py-24 flex items-center justify-center bg-white">
+                <h1> Collections page </h1>
+            </section>
+
+            <ul className="flex items-center justify-center flex-col">
+                {collectionList.map(({ title, id }) => {
+                    return (
+                        <a
+                            className="p-2 hover:underline hover:cursor-pointer"
+                            key={id}
+                            href={`/products/${sanitizeShopifyId(id)}`}
+                        >
+                            {title}
+                        </a>
+                    );
+                })}
+            </ul>
+        </>
+    );
+};
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+    try {
+        const client = Client.buildClient({
+            domain: process.env.SHOPIFY_DOMAIN_NAME,
+            storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+        });
+
+        const collections = await client.collection.fetchAll();
+
+        const collectionList: Collection[] = collections.map(
+            (collection) =>
+                ({
+                    title: collection.title,
+                    id: collection.id,
+                } as Collection)
+        );
+
+        return {
+            props: {
+                collectionList,
+            },
+        };
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export default Collections;
