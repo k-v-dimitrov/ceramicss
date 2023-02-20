a3a54a46c97a19fcd4b91759d3dcf3ea775d6452
Kristiyan Borisov <genkoph@protonmail.com>
2023-02-21 00:58:39 +0200

    fix: update header component story

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -6,6 +6,11 @@ import type HeaderProps from "./header.props";
 export default {
   title: "components/Header",
   component: Header,
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
 };
 
 const Template: Story<HeaderProps> = (props) => <Header {...props} />;
