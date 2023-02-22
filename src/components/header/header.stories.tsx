c768744a32cdbe16d57fab5eebe67f46d933287a
Kristiyan Borisov <genkoph@protonmail.com>
2023-02-22 19:38:46 +0200

    feat: format

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -4,13 +4,13 @@ import Header from "./header";
 import type HeaderProps from "./header.props";
 
 export default {
-  title: "components/Header",
-  component: Header,
-  parameters: {
-    backgrounds: {
-      default: "dark",
+    title: "components/Header",
+    component: Header,
+    parameters: {
+        backgrounds: {
+            default: "dark",
+        },
     },
-  },
 };
 
 const Template: Story<HeaderProps> = (props) => <Header {...props} />;
