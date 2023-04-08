e7e98ba8f0d67b215276f8f1948dd8bb6c2fb2da
Kiril Dimitrov <kir4agi@gmail.com>
2023-04-09 01:13:54 +0300

    chore: rename component

--- src/components/image-rotator/image-rotator.tsx
+++ src/components/image-rotator/image-rotator.tsx
@@ -2,8 +2,8 @@ import { type FC } from "react";
 
 import ImageRotatorProps from "./image-rotator.props";
 
-const Button: FC<ImageRotatorProps> = () => {
+const ImageRotator: FC<ImageRotatorProps> = () => {
     return <h1>image rotator</h1>;
 };
 
-export default Button;
+export default ImageRotator;
