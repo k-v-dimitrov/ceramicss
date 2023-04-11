efe568a0847583c88bf1e81760d9db1f4266f8a9
Kiril Dimitrov <kir4agi@gmail.com>
2023-04-11 20:54:05 +0300

    chore: remove comment

--- src/components/quantity-picker/quantity-picker.props.ts
+++ src/components/quantity-picker/quantity-picker.props.ts
@@ -1,7 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
 
-//(initialState: number | (() => number)): [number, Dispatch<SetStateAction<number>>]
-
 interface QuantityPickerProps {
     currQuantity: number;
     setQuantity: Dispatch<SetStateAction<number>>;
