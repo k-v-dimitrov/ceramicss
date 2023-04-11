966882562f7fffadfeb429e968861214e8caef06
Kiril Dimitrov <kir4agi@gmail.com>
2023-04-11 20:44:36 +0300

    feat: product list item variant

--- src/components/product/list-item/index.tsx
+++ src/components/product/list-item/index.tsx
@@ -1,5 +1,46 @@
-const ListProduct = () => {
-    return <h1>Detailed Product</h1>;
+import Image from "next/image";
+import Link from "next/link";
+import { ProductProps } from "../product.props";
+
+const ListProduct: React.FC<ProductProps> = ({ product }) => {
+    const coverImage = product?.images[0];
+
+    return (
+        <div className="flex flex-row my-10">
+            <span className="mr-4">
+                <Image
+                    src={coverImage?.url}
+                    alt={coverImage?.altText || ""}
+                    width={240}
+                    height={240}
+                />
+            </span>
+
+            <div className="flex-col justify-between w-full">
+                <p className="text-gray-600 text-sm mb-1">{product?.tag}</p>
+
+                <p className="flex justify-between align-center text-lg text-primary-500 font-bold">
+                    {product?.title}
+
+                    <button className="hover:cursor-pointer">
+                        <span
+                            className="icon-remove"
+                            style={{
+                                lineHeight: "unset",
+                            }}
+                        />
+                    </button>
+                </p>
+
+                <p className="my-6 w-3/4">{product?.description}</p>
+
+                <p className="text-gray-700 font-bold">
+                    {product?.variants.amount}
+                    {product?.variants.currencyCode}
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default ListProduct;
