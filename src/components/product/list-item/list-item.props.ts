cbe5ddfc6a8c4fa11453faeb0c84edf15d7846bd
Kiril Dimitrov <kir4agi@gmail.com>
2023-04-13 13:32:00 +0300

    feat: fix line type

--- src/components/product/list-item/list-item.props.ts
+++ src/components/product/list-item/list-item.props.ts
@@ -1,6 +1,11 @@
+import { CartType } from "@/services";
+
+type LineItem = NonNullable<CartType>["lines"][number];
+
 export interface ListItemProps {
     selectedQuantity: number;
     calculatedPrice: number;
     onQuantityUpdate?: (productId: string, quantity: number) => void;
     onProductRemove?: (productId: string) => void;
+    line: LineItem;
 }
