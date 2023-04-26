72185ec6879aa24b274234242f89a74cf7499a7f
Kiril Dimitrov <kir4agi@gmail.com>
2023-04-26 14:55:13 +0300

    feat: delete all cookies

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,7 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            SHOPIFY_STOREFRONT_ENDPOINT: string;
-            SHOPIFY_STOREFRONT_ACCESS_TOKEN: string;
+            NEXT_PUBLIC_SHOPIFY_DOMAIN_NAME: string;
         }
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -27,3 +27,14 @@ export const rebuildShopifyCollectionId = (id: string) => {
 export const rebuildShopifyProductId = (id: string) => {
     return `gid://shopify/Product/${id}`;
 };
+
+export function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
