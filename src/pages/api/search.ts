4f00ccd04d76f27e2240513ee2339cd15fd063b6
Kiril Dimitrov <kir4agi@gmail.com>
2023-04-27 18:38:12 +0300

    feat: search endpoint

---
+++ src/pages/api/search.ts
@@ -0,0 +1,19 @@
+import { Storefront } from "@/services";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "POST") {
+        res.setHeader("Allow", ["POST"]);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+
+    try {
+        const result = await Storefront.products.search(req.body.query);
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({ success: false, error });
+    }
+}
