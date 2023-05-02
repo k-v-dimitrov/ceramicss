f1bbd28ed3c33261979a08eef864e45db09aff17
Kiril Dimitrov <kir4agi@gmail.com>
2023-05-02 17:25:44 +0300

    chore: remove try/catch

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,35 +5,31 @@ import { Storefront } from "@/services";
 export async function middleware(req: NextRequest) {
     const res = NextResponse.next();
 
-    try {
-        if (!req.url.includes("/api")) {
+    if (!req.url.includes("/api")) {
+        return res;
+    }
+
+    const cookie = req.cookies.get("cart");
+
+    if (!cookie) {
+        const cart = await Storefront.cart.create();
+
+        const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
+
+        res.cookies.set("cart", cart!.id, { expires });
+    } else {
+        const isCartValid = await Storefront.cart.getLight(cookie.value);
+
+        if (isCartValid) {
             return res;
         }
 
-        const cookie = req.cookies.get("cart");
+        const cart = await Storefront.cart.create();
 
-        if (!cookie) {
-            const cart = await Storefront.cart.create();
+        const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
 
-            const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
-
-            res.cookies.set("cart", cart!.id, { expires });
-        } else {
-            const isCartValid = await Storefront.cart.getLight(cookie.value);
-
-            if (isCartValid) {
-                return res;
-            }
-
-            const cart = await Storefront.cart.create();
-
-            const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
-
-            res.cookies.set("cart", cart!.id, { expires });
-        }
-
-        return res;
-    } catch (err) {
-        console.error(err);
+        res.cookies.set("cart", cart!.id, { expires });
     }
+
+    return res;
 }
