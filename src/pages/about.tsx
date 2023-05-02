0c96a731887282d2eb0a65ef365b74d6189d3b10
Kiril Dimitrov <kir4agi@gmail.com>
2023-05-02 14:59:19 +0300

    feat: about seo

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,32 +3,72 @@ import Image from "next/image";
 import Head from "next/head";
 
 import { Header, AlternatingSection, Footer } from "@/components";
+import { NextSeo } from "next-seo";
 
 const About: NextPage = () => {
     return (
         <>
-            <Head>
-                <title>Ceramicss - About</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+            <NextSeo
+                title="CeramicsS"
+                description="
+                    CeramicsS е вашият магазин за красиви ръчно изработени керамични изделия. Нашите продукти се изработват с любов и грижа, гарантирайки уникалността и високото качество на всеки един артикул. CeramicsS се фокусира върху устойчивото и етично производство. Разгледайте нашата колекция днес и си донесете допълнително изкуство и елегантност. Не просто магазин за красиви керамични изделия!
+                "
+                openGraph={{
+                    url: "https://ceramicss.eu",
+                    title: "CeramicsS - Онлайн магазин за керамични изделия",
+                    description:
+                        "CeramicsS е вашият магазин за красиви ръчно изработени керамични изделия. Нашите продукти се изработват с любов и грижа, гарантирайки уникалността и високото качество на всеки един артикул. CeramicsS се фокусира върху устойчивото и етично производство. Разгледайте нашата колекция днес и си донесете допълнително изкуство и елегантност. Не просто магазин за красиви керамични изделия!",
+                    images: [
+                        {
+                            url: "/imgs/landing-cover.webp",
+                            width: 800,
+                            height: 600,
+                            alt: "CeramicsS landing",
+                            type: "image/webp",
+                        },
+                    ],
+                    siteName: "CeramicsS",
+                }}
+            />
 
             <Header />
 
             <AlternatingSection
-                SectionImage={<Image src="/imgs/blank.png" height={732} width={732} alt="test" />}
+                SectionImage={
+                    <Image
+                        src="/imgs/blank.png"
+                        height={732}
+                        width={732}
+                        alt="test"
+                    />
+                }
                 title="Test"
                 paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ipsum efficitur, pretium ipsum vitae, interdum massa. Duis elit felis,"
             />
 
             <AlternatingSection
                 flip
-                SectionImage={<Image src="/imgs/blank.png" height={732} width={732} alt="test" />}
+                SectionImage={
+                    <Image
+                        src="/imgs/blank.png"
+                        height={732}
+                        width={732}
+                        alt="test"
+                    />
+                }
                 title="Test"
                 paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ipsum efficitur, pretium ipsum vitae, interdum massa. Duis elit felis,"
             />
 
             <AlternatingSection
-                SectionImage={<Image src="/imgs/blank.png" height={732} width={732} alt="test" />}
+                SectionImage={
+                    <Image
+                        src="/imgs/blank.png"
+                        height={732}
+                        width={732}
+                        alt="test"
+                    />
+                }
                 title="Test"
                 paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ipsum efficitur, pretium ipsum vitae, interdum massa. Duis elit felis,"
             />
