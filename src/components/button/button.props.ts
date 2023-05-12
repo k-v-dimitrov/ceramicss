1d827dbe1061f1aa7c08412b4441397d0cb13f25
Kristiyan Borisov <genkoph@protonmail.com>
2023-05-12 12:19:18 +0300

    refactor: button component

--- src/components/button/button.props.ts
+++
@@ -1,9 +0,0 @@
-import { type ReactNode } from "react";
-
-interface ButtonProps {
-    onClick?: () => void;
-    children: ReactNode | string;
-    className?: string;
-}
-
-export default ButtonProps;
