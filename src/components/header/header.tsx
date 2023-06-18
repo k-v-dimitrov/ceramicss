ed6fed8379663380feecd604a00e364defc015d7
Kristiyan Borisov <genkoph@protonmail.com>
2023-06-19 00:11:15 +0300

    feat: implement mobile menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,59 +1,141 @@
+import clsx from "clsx";
 import Link from "next/link";
+import { useRef } from "react";
+import { usePathname } from "next/navigation";
 
+import { useToggle } from "@/hooks";
 import {
     Logo as LogoIcon,
     Burger as BurgerIcon,
     Magnifier as MagnifierIcon,
 } from "@/components/vectors";
-import CartButton from "@/components/cart-button/cart-button";
+import { CartButton } from "@/components/cart-button";
 
 const links = [
     { label: "Начало", pathname: "/", pattern: /^\/$/ },
     {
         label: "Магазин",
         pathname: "/shop",
-        pattern: /^\/(product|products|cart)(\/[0-9]+)?$/,
+        pattern: /^\/(shop|product|cart)(\/[0-9]+)?$/,
     },
     { label: "За Нас", pathname: "/about", pattern: /\/about/ },
     { label: "Контакти", pathname: "/contacts", pattern: /\/contacts/ },
 ];
 
 function Header() {
+    const pathname = usePathname();
+    const [isSidebarOpen, toggleSidebar] = useToggle();
+
+    // HACK: prevent scrolling when sidebar is open
+    // https://www.jayfreestone.com/writing/locking-body-scroll-ios/
+    const scrollTop = useRef(0);
+
+    const handleToggle = () => {
+        toggleSidebar();
+
+        // this "if" actually fires when the sidebar is open
+        if (!isSidebarOpen) {
+            scrollTop.current = window.scrollY;
+
+            document.body.classList.add("fixed");
+            document.body.classList.add("overflow-hidden");
+            document.body.classList.add("top-0");
+            document.body.classList.add("left-0");
+            document.body.classList.add("right-0");
+            document.body.classList.add("bottom-0");
+            return;
+        }
+
+        document.body.classList.remove("fixed");
+        document.body.classList.remove("overflow-hidden");
+        document.body.classList.remove("top-0");
+        document.body.classList.remove("right-0");
+        document.body.classList.remove("left-0");
+        document.body.classList.remove("bottom-0");
+
+        if (!document.body.classList.length) {
+            document.body.removeAttribute("class");
+        }
+
+        window.scrollTo(0, scrollTop.current);
+    };
+
+    const handleRouteChange = () => {
+        if (isSidebarOpen) {
+            setTimeout(handleToggle, 300);
+        }
+    };
+
     return (
-        <header className="bg-white shadow flex p-4 items-center gap-4 z-20 fixed w-full lg:justify-between h-[var(--header-height)]">
-            <button className="lg:hidden">
-                <BurgerIcon className="w-9 h-8" />
-            </button>
+        <>
+            <header className="bg-white shadow flex p-4 items-center gap-4 z-20 fixed w-full lg:justify-between max-h-[80px]">
+                <button onClick={handleToggle} className="lg:hidden">
+                    <BurgerIcon className="w-9 h-8" />
+                </button>
 
-            <Link href="/">
-                <LogoIcon className="h-9" />
-            </Link>
+                <Link href="/" onClick={handleRouteChange}>
+                    <LogoIcon className="h-9" />
+                </Link>
 
-            <div className="hidden lg:flex gap-16">
-                {links.map((link) => (
-                    <Link
-                        key={link.label}
-                        href={link.pathname}
-                        className="text-primary-500 rounded-xl font-bold"
-                    >
-                        {link.label}
-                    </Link>
-                ))}
-            </div>
-
-            <div className="ml-auto lg:ml-0 flex items-center gap-4">
-                <div className="bg-[#EAEAEA] rounded-full overflow-hidden hidden lg:flex h-12 items-center px-5 gap-4 text-sm">
-                    <input
-                        type="text"
-                        className="bg-transparent w-full focus:outline-none placeholder:text-[#A0A0A0]"
-                        placeholder="Какво търсите?"
-                    />
-                    <MagnifierIcon className="h-6" />
+                <div className="hidden lg:flex gap-16">
+                    {links.map((link) => (
+                        <Link
+                            key={link.label}
+                            href={link.pathname}
+                            className="text-primary-500 rounded-xl font-bold"
+                        >
+                            {link.label}
+                        </Link>
+                    ))}
                 </div>
 
-                <CartButton />
-            </div>
-        </header>
+                <div className="ml-auto lg:ml-0 flex items-center gap-4">
+                    <div className="bg-[#EAEAEA] rounded-full overflow-hidden hidden lg:flex h-12 items-center px-5 gap-4 text-sm">
+                        <input
+                            type="text"
+                            className="bg-unset w-full focus:outline-none placeholder:text-[#A0A0A0] bg-transparent"
+                            placeholder="Какво търсите?"
+                        />
+                        <MagnifierIcon className="h-6" />
+                    </div>
+
+                    <CartButton />
+                </div>
+            </header>
+
+            {isSidebarOpen && (
+                <div className="bg-white fixed top-[80px] w-full h-[calc(100%-80px)] p-3 z-10 lg:hidden">
+                    <div className="bg-[#EAEAEA] rounded-full overflow-hidden flex h-14 items-center px-5 gap-4">
+                        <input
+                            type="text"
+                            className="bg-unset w-full focus:outline-none placeholder:text-[#A0A0A0] bg-transparent"
+                            placeholder="Какво търсите?"
+                        />
+                        <MagnifierIcon className="h-6" />
+                    </div>
+
+                    <div className="flex flex-col mt-4">
+                        {links.map((link) => (
+                            <Link
+                                key={link.label}
+                                href={link.pathname}
+                                onClick={handleRouteChange}
+                                className={clsx(
+                                    "text-lg text-primary-500 py-4 rounded-xl pl-4 font-bold",
+                                    {
+                                        "bg-[#EAEAEA]": link.pattern.test(
+                                            pathname!
+                                        ),
+                                    }
+                                )}
+                            >
+                                {link.label}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
