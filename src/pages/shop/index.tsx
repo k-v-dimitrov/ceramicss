ddb070794fe048234bac8140b06f93cf243d4d7d
Kristiyan Borisov <genkoph@protonmail.com>
2023-07-05 23:07:06 +0300

    feat: implement desktop layout for /shop

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -15,27 +15,34 @@ function Page({ collections }: InferGetStaticPropsType<typeof getStaticProps>) {
                 Магазин
             </h1>
 
-            {collections.filter((collection) => !["Препоръчани"].includes(collection.title)).map((collection, collectionIndex) => (
-                <Fragment key={collection.id}>
-                    <Link href={`/shop/${collection.id}`}>
-                        <Image
-                            src={collection.image?.url}
-                            alt={collection.image?.altText || ""}
-                            height={600}
-                            width={600}
-                            className="brightness-95 rounded-lg w-full mb-4"
-                        />
+            <div className="grid grid-cols-1 sm:gap-6 sm:grid-cols-2 md:grid-cols-3">
+                {collections
+                    .filter(
+                        (collection) =>
+                            !["Препоръчани"].includes(collection.title)
+                    )
+                    .map((collection, idx) => (
+                        <Fragment key={collection.id}>
+                            <Link href={`/shop/${collection.id}`}>
+                                <Image
+                                    src={collection.image?.url}
+                                    alt={collection.image?.altText || ""}
+                                    height={600}
+                                    width={600}
+                                    className="brightness-95 rounded-lg w-full mb-4"
+                                />
 
-                        <p className="text-2xl text-primary-500 text-center">
-                            {collection.title}
-                        </p>
-                    </Link>
+                                <p className="text-2xl text-primary-500 text-center">
+                                    {collection.title}
+                                </p>
+                            </Link>
 
-                    {collectionIndex < collections.length - 1 && (
-                        <hr className="mt-4 mb-7 border-gray-200" />
-                    )}
-                </Fragment>
-            ))}
+                            {idx < collections.length - 1 && (
+                                <hr className="mt-4 mb-7 border-gray-200 sm:hidden" />
+                            )}
+                        </Fragment>
+                    ))}
+            </div>
         </>
     );
 }
